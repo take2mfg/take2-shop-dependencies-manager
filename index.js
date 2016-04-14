83092290515c89c61b89ebcf284f4aac4576b332
Andres Serrano <andres.srg@gmail.com>
2016-04-14 15:00:07 -0500

    Add needed dependencies that require fastboot check

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 /* jshint node: true */
 'use strict';
 
+
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
+
+
 module.exports = {
-  name: 'take2-shop-dependencies-manager'
+  name: 'take2-shop-dependencies-manager',
+
+  included: function(app) {
+    this._super.included(app);
+
+    if (!isFastBoot()) {
+      app.import(app.bowerDirectory + '/localforage/dist/localforage.js');
+      app.import(app.bowerDirectory + '/jquery.payment/lib/jquery.payment.js');
+      app.import(app.bowerDirectory + '/sweetalert/dist/sweetalert.min.js');
+      app.import(app.bowerDirectory + '/sweetalert/dist/sweetalert.css');
+      app.import(app.bowerDirectory + '/FitText.js/jquery.fittext.js');
+
+      // bootstrap libs
+      [
+        'tooltip',
+        'popover',
+        'dropdown',
+        'collapse'
+      ].forEach(function(lib) {
+        app.import(app.bowerDirectory + '/bootstrap-sass/assets/javascripts/bootstrap/' + lib + '.js');
+      });
+    }
+  }
 };
